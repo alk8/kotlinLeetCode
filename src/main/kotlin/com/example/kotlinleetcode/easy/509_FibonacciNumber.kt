daffdfc90468e7784699f415c688581c338deb37
alk <l3pxbsk2kh>
2023-06-11 16:00:00 +0300

    2 task

---
+++ src/main/kotlin/com/example/kotlinleetcode/easy/509_FibonacciNumber.kt
@@ -0,0 +1,19 @@
+package com.example.kotlinleetcode.easy
+
+fun main() {
+
+    fib(2)
+
+}
+
+fun fib(n: Int): Int {
+
+    if (n==0) return 0
+
+    var previous = 0
+    var num = 1
+
+    for (i in 1 until n) num += previous.also { previous = num }
+    return num
+
+}
